01fe6d1e111787a56d608064c352a7d0573ac108
Sushank N S <sushank82000@gmail.com>
2021-04-24 12:10:05 +0530

    text

--- SignUp.js
+++ SignUp.js
@@ -3,9 +3,14 @@ var app = express();
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
+var nodemailer = require('nodemailer');
 //app.use(express.static("public"));
 //app.use(express.static(__dirname + '/public'));
-var url = "mongodb link";
+var url = "";
+
+var binary = require('mongodb').Binary;
+var fileUpload = require('express-fileupload'); 
+app.use(fileUpload());
 
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -15,6 +20,9 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
 var name = "";
+var otp = 0;
+var otp1 = "";
+var mailid = "";
 
 app.post('/SignUp', urlencodedParser, function (req, res) {
 if(req.body.password==req.body.confirmpassword)
@@ -64,14 +72,14 @@ app.post('/SignIn', urlencodedParser, function (req, res) {
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    name = req.body.username
-    var query = { UserName: req.body.username, Password: req.body.password };
+    name = req.body.username1
+    var query = { UserName: req.body.username1, Password: req.body.password1 };
     dbo.collection("users").find(query).toArray(function(err, result) {
       //if (err) throw err;
       //console.log(result);
       if(result[0]!=null)
       {
-        res.render('home2',{n:req.body.username});
+        res.render('home2',{n:req.body.username1,x:0});
         db.close();
       }
       else
@@ -104,46 +112,149 @@ app.post('/ML', urlencodedParser, function (req, res) {
 })
 
 app.post('/OD1', urlencodedParser, function (req, res) {
+  if(req.body.FromDate<req.body.ToDate){
   MongoClient.connect(url, function(err, db) {
     //if (err) throw err;
     var dbo = db.db("mydb");
     var myobj = { Name: name, LeaveType: "OD", FromDate: req.body.FromDate, FromTime: req.body.FromTime, ToDate: req.body.ToDate, ToTime: req.body.ToTime, EventType: req.body.EventType, ParticipationType: req.body.ParticipationType, Award: req.body.Award };
     dbo.collection("leave").insertOne(myobj, function(err, rest) {
       console.log("1 document inserted");
-      res.render('home2',{n:name});
+      res.render('home2',{n:name,x:1});
       db.close();
       res.end();
     });
-  })  
+  })
+}  
+else{
+  res.render('od');
+}
 })
 
 app.post('/OL1', urlencodedParser, function (req, res) {
+  if(req.body.FromDate<req.body.ToDate){
   MongoClient.connect(url, function(err, db) {
     //if (err) throw err;
     var dbo = db.db("mydb");
     var myobj = { Name: name, LeaveType:"OL", FromDate: req.body.FromDate, FromTime: req.body.FromTime, ToDate: req.body.ToDate, ToTime: req.body.ToTime, Reason: req.body.Reason };
     dbo.collection("leave").insertOne(myobj, function(err, rest) {
       console.log("1 document inserted");
-      res.render('home2',{n:name});
+      res.render('home2',{n:name,x:1});
       db.close();
       res.end();
     });
-  })  
+  })
+}  
+  else{
+    res.render('ol');
+  }
 })
 
 app.post('/ML1', urlencodedParser, function (req, res) {
+  if(req.body.FromDate<req.body.ToDate){
   MongoClient.connect(url, function(err, db) {
     //if (err) throw err;
     var dbo = db.db("mydb");
     var myobj = { Name: name, LeaveType:"ML", FromDate: req.body.FromDate, FromTime: req.body.FromTime, ToDate: req.body.ToDate, ToTime: req.body.ToTime, Type: req.body.Type, TreatmentDetails: req.body.TreatmentDetails };
     dbo.collection("leave").insertOne(myobj, function(err, rest) {
       console.log("1 document inserted");
-      res.render('home2',{n:name});
+      //res.render('home2',{n:name,x:1});
+      //db.close();
+      //res.end();
+    });
+    var file = {Name: name, File: binary(req.files.myFile.data)}
+    dbo.collection("files").insertOne(file, function(err, rest) {
+      res.render('home2',{n:name,x:1});
       db.close();
       res.end();
     });
-  })  
+  }) 
+} 
+else{
+  res.render('ml');
+}
 })
 
+app.post('/forgot', function (req, res) {
+  res.render('otpsend');
+})
+
+app.post('/OTPSend', function (req, res) {
+  otp = Math.floor(Math.random() * (999 - 101) + 101);
+  otp1 = otp.toString();
+  mailid=req.body.email;
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var query = { MailID: req.body.email };
+    dbo.collection("users").find(query).toArray(function(err, result) {
+      if(result[0]!=null)
+      {
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'project18141@gmail.com',
+            pass: 'admin141$'
+          }
+        });
+        
+        var mailOptions = {
+          from: 'project18141@gmail.com',
+          to: req.body.email,
+          subject: 'OTP',
+          text: otp1
+        };
+        
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+      
+        res.render('otpverify');
+        db.close();
+      }
+      else
+      {
+        res.render('SignUpIn',{n:6});
+      }
+      //db.close();
+    });
+  });
+
+})
+
+app.post('/OTPverify', function (req, res) {
+  if(req.body.otp2 == otp1)
+  {
+    res.render('forgotchangepassword');
+  }
+  else
+  {
+    res.render('otpverify');
+  }
+})
+
+app.post('/OTPconfirm', function (req, res) {
+  if(req.body.confirmpassword == req.body.password)
+  {
+    MongoClient.connect(url, function(err, db) {
+      var dbo = db.db("mydb");
+      var myquery = { MailID: mailid };
+      var newvalues = { $set: {Password: req.body.password } };
+      dbo.collection("users").updateOne(myquery, newvalues, function(err, rest) {
+        console.log("1 document updated");
+        db.close();
+        res.render('SignUpIn',{n:5});
+        res.end();
+      });
+    });
+  }
+  else
+  {
+    res.render('SignUpIn',{n:7});
+  }
+})
 
 var server = app.listen(8081, function () {});
